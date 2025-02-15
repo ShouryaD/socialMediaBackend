5e5bb1b92f47ad4457e354d5469c06b6521c9ca8
ShouryaD <dwivedishoury@gmail.com>
2025-02-15 23:44:26 +0530

    db bug fix 2

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-console.log(process.env.db)
+
 let connection = ()=>{
     mongoose.connect(process.env.db)
     .then(()=>console.log('Connected to MongoDB'))
