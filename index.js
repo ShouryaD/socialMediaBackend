e780e3ee6e38446981649bd49dfb32b289703e37
ShouryaD <dwivedishoury@gmail.com>
2025-02-16 15:56:57 +0530

    cors changes

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ const messageRoutes = require('./routes/messageRoutes')
 app.set('view engine', 'ejs')
 
 //middleware
-app.use(cors())
+app.use(cors({
+  origin: 'social-media-frontend-psi-one.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use(express.json({ limit: "50mb" }));
 app.use("/users", userRoutes);
 app.use('/posts', postRoutes)
